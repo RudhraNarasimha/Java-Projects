27ed507f7c6d5c7f83bf246cc22b31e6975dc06d
user <user@DESKTOP-OC2FCPH>
2025-06-06 14:50:12 +0530

    BankManagementSystem

--- BankingManagementSystem/src/com/project/services/BankingService.java
+++ BankingManagementSystem/src/com/project/services/BankingService.java
@@ -1,10 +1,13 @@
 package com.project.services;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import com.project.model.Account;
+import com.project.model.CurrentAccount;
 import com.project.model.Customer;
+import com.project.model.SavingAccount;
 
 public class BankingService {
 	private Map<String, Customer> customers = new LinkedHashMap<>();
@@ -14,7 +17,7 @@ public class BankingService {
 		if (customers.containsKey(id)) {
 			System.out.println("Customer id already exist");
 		} else {
-			Customer customer = new Customer();
+			Customer customer = new Customer(id, name);
 			customers.put(id, customer);
 			System.out.println("Customer Added Successfully");
 		}
@@ -22,6 +25,69 @@ public class BankingService {
 	}
 
 	public void createAccount(String acc_type, String acc_number, String cus_id, double initial_deposit) {
+		if (customers.containsKey(cus_id)) {
+			if (accounts.containsKey(acc_number)) {
+				System.out.println("Account Already Exists.................");
+			} else {
+				if (acc_type.equalsIgnoreCase("savings")) {
+					Customer customer = customers.get(cus_id);
+					Account account = new SavingAccount(acc_number, null, initial_deposit);
+					accounts.put(acc_number, account);
+					System.out.println("Savings Account Created Successfully");
+				} else if (acc_type.equals("current")) {
+					Customer customer = customers.get(cus_id);
+					Account account = new CurrentAccount(acc_number, null, initial_deposit);
+					accounts.put(acc_number, account);
+					System.out.println("Current Account Created Successfully");
+				} else {
+					System.out.println("invalid Account type");
+				}
+			}
+		} else {
+			System.out.println("Invalid Customer Id....................");
+		}
+	}
+
+	public void deposit(String dep_acc_num, double dep_amount) {
+		if (accounts.containsKey(dep_acc_num)) {
+			Account account = accounts.get(dep_acc_num);
+			account.deposit(dep_amount);
+			System.out.println("Amount Deposited Successfully");
+			System.out.println("Available Balance" + account.getBalance());
+
+		} else {
+			System.out.println("Invalid Account Number............... ");
+		}
+	}
+
+	public void withdraw(String withdraw_acc_num, double withdraw_amount) {
+		if (accounts.containsKey(withdraw_acc_num)) {
+			Account account = accounts.get(withdraw_acc_num);
+			account.withdraw(withdraw_amount);
+			System.out.println("Amount Withdrawan Successfully");
+			System.out.println("Available Balance" + account.getBalance());
+		} else {
+			System.out.println("Invalid Withdraw Account Number............... ");
+		}
+	}
+
+	public void checkBalance(String check_acc_num) {
+		if (accounts.containsKey(check_acc_num)) {
+			Account account = accounts.get(check_acc_num);
+			double balance = account.getBalance();
+			System.out.println("Available Balance : " + balance);
+		} else {
+			System.out.println("Invalid Withdraw Account Number............... ");
+		}
 
 	}
+
+	public void getAccounts() {
+		Collection<Account> acclist = accounts.values();
+		for (Account obj : acclist) {
+			System.out.println(obj);
+		}
+
+	}
+
 }
--- BankingManagementSystem/src/com/project/view/BankingManagementSystem.java
+++ BankingManagementSystem/src/com/project/view/BankingManagementSystem.java
@@ -9,19 +9,20 @@ public class BankingManagementSystem {
 		Scanner scanner = new Scanner(System.in);
 		BankingService service = new BankingService();
 		while (true) {
-			System.out.println("*");
-			System.out.println("*");
-			System.out.println("*Banking Management System*");
-			System.out.println("*");
-			System.out.println("*");
-			System.out.println("*1.Add Customer");
-			System.out.println("*2.Add Account*");
-			System.out.println("*3.Deposit Amount");
-			System.out.println("*4.Withdraw Amount*");
-			System.out.println("*5.Check Balance*");
-			System.out.println("*6.View Accounts*");
-			System.out.println("*");
-			System.out.println("*");
+			System.out.println("*****************************");
+			System.out.println("*****************************");
+			System.out.println("Banking Management System");
+			System.out.println("*****************************");
+			System.out.println("*****************************");
+			System.out.println("1.Add Customer");
+			System.out.println("2.Add Account");
+			System.out.println("3.Deposit Amount");
+			System.out.println("4.Withdraw Amount");
+			System.out.println("5.Check Balance");
+			System.out.println("6.View Accounts");
+			System.out.println("7.Exit");
+			System.out.println("****************************");
+			System.out.println("****************************");
 
 			int choice = scanner.nextInt();
 			switch (choice) {
@@ -42,6 +43,39 @@ public class BankingManagementSystem {
 				System.out.println("Enter Initial Deposit");
 				double initial_deposit = scanner.nextDouble();
 				service.createAccount(acc_type, acc_number, cus_id, initial_deposit);
+				break;
+
+			case 3:
+				System.out.println("Enter Account Number : ");
+				String dep_acc_num = scanner.next();
+				System.out.println("Enter Deposit Amount : ");
+				double dep_amount = scanner.nextDouble();
+				service.deposit(dep_acc_num, dep_amount);
+
+				break;
+
+			case 4:
+				System.out.println("Enter Withdraw Account Number : ");
+				String withdraw_acc_num = scanner.next();
+				System.out.println("Enter Withdraw Amount : ");
+				double withdraw_amount = scanner.nextDouble();
+				service.withdraw(withdraw_acc_num, withdraw_amount);
+				break;
+
+			case 5:
+				System.out.println("Enter Account Number : ");
+				String check_acc_num = scanner.next();
+				service.checkBalance(check_acc_num);
+				break;
+
+			case 6:
+				service.getAccounts();
+				break;
+			case 7:
+				System.exit(0);
+
+			default:
+				System.out.println("Please Enter Valid Choice");
 				break;
 			}
 		}
